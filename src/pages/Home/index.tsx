6211a50835c5fef3330dad71f29fb36d6192a807
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-03-05 18:09:01 -0300

    preparação do projeto

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>pagina home</h1>
+
+        </div>
+    );
+}
+
+export default Home;
