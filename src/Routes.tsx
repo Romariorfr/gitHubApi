0672c313a329f4faf2ceb602890c852fc677c6c4
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-03-06 17:13:56 -0300

    navbar

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'pages/Home';
+import Navbar from 'components/Navbar';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Navbar/>
       <Route path="/" exact>
         <Home />
       </Route>
