1f92197e1c9745070fcacda6651809b37ab6abc3
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-03-07 19:35:10 -0300

    handleChange input

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,22 +1,50 @@
 import Button from 'components/Navbar/Button';
+import { useState } from 'react';
 import './styles.css';
 
+type GitHubData = {
+  photo: string;
+  profile: string;
+  followers: string;
+  locations: string;
+  name: string;
+};
+
+type FormData = {
+  userName: string;
+};
+
 const Profile = () => {
+  const [gitHubData, setGitHubData] = useState<GitHubData>();
+
+  const [formData, setFormData] = useState<FormData>({ userName: '' });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+    console.log(formData);
+  };
+
   return (
     <div className="profile-container">
-
       <div className="card-container">
         <h1>Encontre um perfil GutHub</h1>
-        <input placeholder='Usuário Github'/>
+        <input
+          name="userName"
+          onChange={handleChange}
+          placeholder="Usuário Github"
+        />
         <Button text="Encontrar" />
       </div>
 
       <div className="card-info">
         <div className="img-github">
-          <p >imagem</p>
+          <p>imagem</p>
         </div>
         <div className="data-github">
-          <p className='title-info'>informações</p>
+          <p className="title-info">informações</p>
           <div>
             <h4>Perfil:</h4>
             <p className="color-link">https://api.github.com/users/acenelio</p>
@@ -35,7 +63,6 @@ const Profile = () => {
           </div>
         </div>
       </div>
-
     </div>
   );
 };
