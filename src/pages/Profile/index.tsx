120f32c23a4fd5994475c008b5ac4ce04524866d
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-03-06 21:50:29 -0300

    card-info

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,11 +3,23 @@ import './styles.css';
 
 const Profile = () => {
   return (
-    <div className='profile-container'>
+    <div className="profile-container">
       <div className="card-container">
         <h1>Encontre um perfil GutHub</h1>
         <input />
         <Button text="Encontrar" />
+      </div>
+      <div className="card-info">
+        <div className="img-github">
+          <p>imagem</p>
+        </div>
+        <div className="data-github">
+          <p>informações</p>
+          <div>Perfil:</div>
+          <div>Seguidores:</div>
+          <div>Localidade:</div>
+          <div>Nome:</div>
+        </div>
       </div>
     </div>
   );
