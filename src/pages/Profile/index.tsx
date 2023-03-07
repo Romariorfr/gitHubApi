795e13973bbaf62324831d52ce929fae1da88613
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-03-07 19:06:26 -0300

    layout finish

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,23 +4,38 @@ import './styles.css';
 const Profile = () => {
   return (
     <div className="profile-container">
+
       <div className="card-container">
         <h1>Encontre um perfil GutHub</h1>
-        <input />
+        <input placeholder='Usuário Github'/>
         <Button text="Encontrar" />
       </div>
+
       <div className="card-info">
         <div className="img-github">
-          <p>imagem</p>
+          <p >imagem</p>
         </div>
         <div className="data-github">
-          <p>informações</p>
-          <div>Perfil:</div>
-          <div>Seguidores:</div>
-          <div>Localidade:</div>
-          <div>Nome:</div>
+          <p className='title-info'>informações</p>
+          <div>
+            <h4>Perfil:</h4>
+            <p className="color-link">https://api.github.com/users/acenelio</p>
+          </div>
+          <div>
+            <h4>Seguidores:</h4>
+            <p>4379</p>
+          </div>
+          <div>
+            <h4>Localidade:</h4>
+            <p>Uberlândia</p>
+          </div>
+          <div>
+            <h4>Nome:</h4>
+            <p>Nelio Alvez</p>
+          </div>
         </div>
       </div>
+
     </div>
   );
 };
